ad3d5596e47ce282f4bbb96b8f7ab6a0fcfe2bdf
Tracey <tc400617@ohio.edu>
2021-09-15 15:23:26 -0400

    last part of lab 2

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Tracey Copeland
+ *        Date: 09/15/21
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello git!" << endl;
+    return 0;
+}// main
